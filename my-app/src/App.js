f03625ae5b54042962dddd587043368fa5b18b05
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-05-20 13:05:53 +0530

    adding one more component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,29 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
-
+import Alert from './components/Alert';
  
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert,setAlert] = useState(null)
+
+  const showAlert =(message,type)=>{
+    setAlert({
+      msg : message,
+      type : type
+  })
+  }
 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode has been enabled","Success")
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Dark Mode has been enabled","Success")
     }
   }
   return (
@@ -22,6 +32,7 @@ function App() {
     {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
     {/* <Navbar/> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
     <div className="container my-3">
     <TextForm heading="Enter the text to analyze below" mode={mode}/>
     </div>
