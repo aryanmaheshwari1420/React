1e5fa4f955a318558231b1a19eb88445efa023ab
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-05-25 22:17:25 +0530

    pushing new content

--- newsApp/my-app/src/components/News.js
+++ newsApp/my-app/src/components/News.js
@@ -1,57 +1,139 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./spinner";
 
-import NewsItem from './NewsItem'
-export class News extends Component {
-
-  constructor(){
-    super();
+class News extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      articles : [],
-      loading  : false
-    }
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0,
+    };
   }
 
   async componentDidMount() {
     try {
-      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=18c202d4151d4ac6ac2dabe02c3f7060";
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=18c202d4151d4ac6ac2dabe02c3f7060&page=1&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
       let response = await fetch(url);
       if (!response.ok) {
         throw new Error("Failed to fetch news articles.");
       }
       let parsedData = await response.json();
       console.log(parsedData);
-      this.setState({ articles: parsedData.articles });
+      this.setState({
+        articles: parsedData.articles,
+        totalResults: parsedData.totalResults,
+        loading: false,
+      });
     } catch (error) {
       console.log(error);
       // Handle error state or display an error message to the user
     }
   }
-  
+
+  handleOnNext = async () => {
+    if (
+      this.state.page + 1 <=
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
+      try {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=18c202d4151d4ac6ac2dabe02c3f7060&page=${
+          this.state.page + 1
+        }&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        let response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Failed to fetch news articles.");
+        }
+        let parsedData = await response.json();
+        console.log(parsedData);
+        this.setState({
+          articles: parsedData.articles,
+          page: this.state.page + 1,
+          loading: false,
+        });
+      } catch (error) {
+        console.log(error);
+        // Handle error state or display an error message to the user
+      }
+    }
+  };
+
+  handleOnPrev = async () => {
+    if (this.state.page > 1) {
+      try {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=18c202d4151d4ac6ac2dabe02c3f7060&page=${
+          this.state.page - 1
+        }&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        let response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Failed to fetch news articles.");
+        }
+        let parsedData = await response.json();
+        console.log(parsedData);
+        this.setState({
+          articles: parsedData.articles,
+          page: this.state.page - 1,
+          loading: false,
+        });
+      } catch (error) {
+        console.log(error);
+        // Handle error state or display an error message to the user
+      }
+    }
+  };
 
   render() {
     const { articles, loading } = this.state;
-  
+
     return (
-      <div className='container my-3'>
-        <h2>News - Top Headlines</h2>
-        <div className='row'>
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
+      <div className="container my-3">
+        <h1 className="text-center">News-Top Headlines</h1>
+        {loading && <Spinner />}
+
+        <div className="row">
+          {!loading &&
             articles.map((element) => (
-              <div className='col-md-4' key={element.url}>
+              <div className="col-md-4" key={element.url}>
                 <NewsItem
                   title={element.title ? element.title.slice(0, 45) : ""}
-                  description={element.description ? element.description.slice(0, 88) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 88) : ""
+                  }
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
                 />
               </div>
-            ))
-          )}
+            ))}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            disabled={this.state.page <= 1}
+            onClick={this.handleOnPrev}
+            className="btn btn-light"
+          >
+            ← Previous
+          </button>
+          <button
+            type="button"
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            onClick={this.handleOnNext}
+            className="btn btn-light"
+          >
+            Next →
+          </button>
         </div>
       </div>
     );
   }
 }
-export default News
+
+export default News;
--- newsApp/my-app/src/components/NewsItem.js
+++ newsApp/my-app/src/components/NewsItem.js
@@ -4,13 +4,14 @@ export class NewsItem extends Component {
   render() {
     let {title,description,imageUrl, newsUrl} = this.props;
     return (
-      <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+      <div className="my-3" >
+        <div className="card" 
+        style={{ width: "18rem"  }}>
           <img src={!imageUrl?"https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2022/07/what-is-a-url.webp":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">
+            <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-info">
               Read More
             </a>
           </div>
