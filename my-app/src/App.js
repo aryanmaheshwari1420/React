5f63acc3df37ff29afaf3760bc182feaba7effe1
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-12-27 22:00:28 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import About from './components/About';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  const [mode, setMode]   = useState("light"); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode has been enabled", "Success");
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "Success");
+      document.title = "TextUtils - Light Mode";
+    }
+  };
+
+  return (
+    <Router>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert = {alert}/>  
+      <div className="container my-3">
+        <Routes>   
+          <Route exact path="/about" element={<About mode={mode}/>} />
+          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" showAlert={showAlert}/>} />
+        </Routes>
+      </div>
+   </Router>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/cam.js
@@ -0,0 +1,15 @@
+import React from "react";
+import React, { memo } from 'react'
+
+export default memo(function cam() {
+  return (
+    <div>
+      <video>
+        id = "webcam"
+        <style>
+            
+        </style>
+      </video>
+    </div>
+  )
+})
